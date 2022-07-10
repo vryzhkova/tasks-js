fa08e6775537c6be24f4b32f4f11fd7965a049fc
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-07-10 16:36:20 +0400

    added task 52

--- 52-default-function-parameters/finish.js
+++ 52-default-function-parameters/finish.js
@@ -14,6 +14,14 @@
  * Внимательно сравните свои результаты с результатами тестовых вызовов
  */
 
+//  const weatherForecast = (city, weather) => {
+//     return `Прогноз погоды для города ${city}: ${weather}`;
+// }
+
+const weatherForecast = (city, weather = 'Отличная погода!') => {
+    return `Прогноз погоды для города ${city}: ${weather}`;
+}
+
 console.log(weatherForecast('Dubai', 'Солнечно'))
 // Прогноз погоды для города Dubai: Солнечно
 
