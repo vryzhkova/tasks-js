bbd239efb74e984f19b7fca40c39d6be5828d12b
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-07-10 15:45:18 +0400

    added task 51

--- 51-rest-and-spread-operators/finish.js
+++ 51-rest-and-spread-operators/finish.js
@@ -13,21 +13,61 @@
  * так и оператор "spread".
  */
 
+
+// Мое решение
+
+// const meanScore = (arr) => {
+//   let sum = 0;
+  
+//   for (let i = 0; i < arr.length; i++) {
+//     if(typeof arr[i] !== 'number') {
+//       return 'Все аргументы в вызове функции должны быть числами!';
+//     } 
+//      sum += arr[i];
+//   }
+//   return (sum / arr.length).toFixed(2);
+// }
+
+// Решение учителя
+
+function meanScore(...numbers) {
+  if (numbers.some((num) => typeof num !== 'number')) {
+    console.error('Все аргументы в вызове функции должны быть числами!');
+    return
+    //throw new Error ('Все аргументы в вызове функции должны быть числами!');
+    //return 'Все аргументы в вызове функции должны быть числами!';
+  }
+
+/**
+ * num1 / numbers.length + num2 / numbers.length + ... + num10 / numbers.length
+ * РАВНОСИЛЬНО
+ * (num1 + num2 + ... + num10) / numbers.length;
+ */
+
+  return numbers
+    .reduce((mean, num) => mean + num / numbers.length, 0)
+    .toFixed(2)
+}
+
 const scores1 = [0, 1.5, 2.5, 3.7]
 const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
 const scores3 = [1.3, 2.5, 1.9]
 const scores4 = ['abc', 1.3, true, 2.5, 1.9]
 
-console.log(meanScore(/* все элементы из массива "scores1" */))
+//console.log(meanScore(/* все элементы из массива "scores1" */))
+console.log(meanScore(...scores1));
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+//console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+console.log(meanScore(...scores1, ...scores2));
 // 2.8
 
-console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
-)
+// console.log(
+//   meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
+console.log(meanScore(...scores1, ...scores2, ...scores3));
+// )
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */))
+//console.log(meanScore(/* все элементы из массива "scores4" */))
+console.log(meanScore(...scores4));
 // Все аргументы в вызове функции должны быть числами!
