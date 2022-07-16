53f802874ebfc35f9499113d31482df8ee0838a5
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-07-16 19:49:25 +0400

    added task 57

--- 57-array-transformation/finish.js
+++ 57-array-transformation/finish.js
@@ -9,6 +9,44 @@
  * 3. Исходный массив постов должен остаться без изменений
  */
 
+// Мое решение (неправильное)
+// const processPosts = (arr) => {
+//   const processPosts = (arr) => {
+//     let copyOfArr = [...arr];
+  
+//     for(let i = 0; i < arr.length; i++) {
+//       copyOfArr[i].postId += 1000;
+//     }
+    
+//      return copyOfArr;
+//   }
+// }
+
+// Решение учителя
+
+// const processPosts = (posts) => {
+//   return posts.map((post) => {
+//     return {
+//       postId: post.postId + 1000,
+//       postAuthor: post.author,
+//       postCommentsQty: post.commentsQty === undefined ? 0 : post.commentsQty,
+//     }
+//   })
+// }
+
+// OptimizationCode
+
+const processPosts = (posts) => {
+  return posts.map((post) => {
+    const { postId, author: postAuthor, commentsQty: postCommentsQty = 0} = post;
+    return {
+      postAuthor,
+      postCommentsQty,
+      postId: postId + 1000,
+    }
+  })
+}
+
 const testPosts = [
   {
     postId: 234,
