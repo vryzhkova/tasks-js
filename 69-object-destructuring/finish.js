59b77f5b24aad84f03289ad6480ee1e91a7f175e
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-08-01 15:05:14 +0400

    added task 69

--- 69-object-destructuring/finish.js
+++ 69-object-destructuring/finish.js
@@ -12,7 +12,27 @@ const objectWithNumbers = {
   z: 3,
 }
 
-// Создайте функцию здесь
+// Мое решение
+// function mult(objectWithNumbers) {
+//   const values = Object.values(objectWithNumbers);
+//   let result = 1;
+
+//   for(const value of values) {
+//     result *= value;
+//   }
+  
+//   return result;
+// }
+
+// Решение учителя 1
+// const mult = (nums) => {
+//   const { x, y, z } = nums;
+
+//   return x * y * z;
+// }
+
+// Решение учителя 2
+const mult = ({ x, y, z }) =>  x * y * z;
 
 const result = mult(objectWithNumbers)
 console.log(result)
