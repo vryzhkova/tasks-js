a6c00d83079c945eacc86054cffddbd714f8cae5
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-08-01 22:13:53 +0400

    added task 75

--- 75-iterate-over-string/finish.js
+++ 75-iterate-over-string/finish.js
@@ -9,7 +9,28 @@ const vowels = ['a', 'e', 'i', 'o', 'u']
 
 const str = 'Today is the best day of my life'
 
-// Напишите код здесь
+// Мое решение
+// str.split('').forEach((item) => {
+//   for(let i of vowels) {
+//        if (item === i) {
+//       vowelsCount += 1;
+//     }
+//   }
+// })
+
+// Решение учителя
+// str.split('').forEach((item) => {
+//     if (vowels.includes(item)) {
+//     vowelsCount += 1;
+//     }
+// })
+
+// Решение учителя 2
+for (const item of str) {
+    if (vowels.includes(item)) {
+        vowelsCount += 1;
+    }
+}
 
 console.log(vowelsCount)
 // 9
